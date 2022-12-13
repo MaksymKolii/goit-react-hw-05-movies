ac474a36afcfb35d99d1be1967aa4395018da55c
MaksymKolii <maksymkolii@gmail.com>
2022-12-13 23:31:18 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,29 @@
+
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import {Movies} from './Pages/Movies'
+import { NotFound } from "./Pages/NotFound";
+import { Container, Header, Link } from "./App.styled";
 
 export const App = () => {
   return (
-    <div>
-    <Movies/>
-    </div>
+    <Container>
+      <Header>
+      <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movie">Movies</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+      <Movies/>
+    </Container>
   );
 };
