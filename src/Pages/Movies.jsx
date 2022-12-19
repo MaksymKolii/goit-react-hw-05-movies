eb39060dcd3744591d97ad52c200b4dfd6c706d3
Maksym Kolii <maksymkolii@gmail.com>
2022-12-19 18:45:46 +0100

    commit

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Api from '../Services/apiFetcher';
@@ -6,9 +6,15 @@ import Api from '../Services/apiFetcher';
 export const Movies = () => {
   const [movies, setMovies] = useState([]);
   let [searchParams, setSearchParams] = useSearchParams();
+  const isFirstRender = useRef(true);
   const query = searchParams.get('moviename');
 
   useEffect(() => {
+    if (!query) {
+      // alert('enter something');
+      return;
+    }
+
     async function getMovies() {
       try {
         const res = await Api.fetchMoviesByName(query);
@@ -18,14 +24,34 @@ export const Movies = () => {
       }
     }
     getMovies();
+    if (isFirstRender.current) {
+      console.log(isFirstRender);
+      isFirstRender.current = false;
+      return;
+    }
   }, [query]);
 
   console.log(movies);
 
+  const handleSubmit = ev => {
+    ev.preventDefault();
+    const form = ev.target;
+    if (query && query.trim() === '') {
+      return alert('Put something in input');
+    }
+    setSearchParams({ moviename: form.elements.something.value });
+    form.reset();
+  };
+
   return (
     <main>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="something" />
+        <button>Search</button>
+      </form>
       <ul>
         {movies &&
+          query !== null &&
           movies.map(({ id, title }) => (
             <li key={id}>
               <Link to={`/movies/${id}`}>{title}</Link>
