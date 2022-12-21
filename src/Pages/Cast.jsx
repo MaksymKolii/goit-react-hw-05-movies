b4037e5a34894eb74df2dfe7e21b68efdbe0a4c4
Maksym Kolii <maksymkolii@gmail.com>
2022-12-21 15:52:36 +0100

    commit

--- src/Pages/Cast.jsx
+++ src/Pages/Cast.jsx
@@ -1,13 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Api from '../Services/apiFetcher';
+import { CastList } from 'components/CastList/CastList';
 
 export const Cast = () => {
   const [cast, setCast] = useState(null);
 
   const { movieId } = useParams();
-
-  // const location = userLocation();
 
   useEffect(() => {
     async function getActors() {
@@ -21,35 +20,5 @@ export const Cast = () => {
     getActors();
   }, [movieId]);
 
-  console.log(cast);
-  const genderSwitcher = data => {
-    if (data === 2) {
-      return 'Actor';
-    }
-    if (data === 1) {
-      return 'Actress';
-    }
-  };
-  return (
-    <>
-      <ul>
-        {cast &&
-          cast.map(({ cast_id, name, gender, profile_path }) => (
-            <li key={cast_id}>
-              <img
-                src={
-                  profile_path
-                    ? 'https://image.tmdb.org/t/p/w500' + profile_path
-                    : 'https://louisville.edu/history/images/noimage.jpg/image'
-                }
-                alt={name}
-                width="100"
-              ></img>
-              <p>{genderSwitcher(gender)}</p>
-              <span>{name}</span>
-            </li>
-          ))}
-      </ul>
-    </>
-  );
+  return <>{cast && <CastList cast={cast} />}</>;
 };
--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
+
 export const Genres = ({ genreArray }) => {
+  const commaGenres = data => {
+    if (data === null) return;
+    return data.map(gen => gen.name).join(', ');
+  };
   return (
-    <ul>
-      {genreArray.map(({ id, name }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
+    <p>{`${commaGenres(genreArray)}`}</p>
+    // <ul>
+    //   {genreArray.map(({ id, name }) => (
+    //     <li key={id}>{name}</li>
+    //   ))}
+    // </ul>
   );
 };
 Genres.propTypes = {
